a5fa05ca8064e5a68c23e9a7b5af1da7290f5b3d
Sander Nordhagen Svingen <sandersvingen98@gmail.com>
2022-10-04 03:20:14 +0200

    All tests passed 🤗

---
+++ script.js
@@ -0,0 +1,35 @@
+//
+//
+//  File created 03.10.2022 23:31
+//  By Sander Nordhagen Svingen
+//  sanderns on Github
+//
+//
+
+const baseAmount = 111477;
+const workDays = 260;
+
+const checkUser = (y21, y20, y19) => {
+	// Variables
+	const totalIncome = y21 + y20 + y19;
+	const averageIncome = totalIncome / 3;
+	const highestIncome = Math.max(y21, averageIncome);
+
+	// Booleans
+	const check15G = y21 > baseAmount * 1.5;
+	const check3G = totalIncome > baseAmount * 3;
+	const check6G = highestIncome > baseAmount * 6;
+
+    // Guard clauses
+	if (!check15G && !check3G) {
+		return 0;
+	}
+
+	if (check6G) {
+		return Math.ceil((baseAmount * 6) / workDays);
+	}
+
+	return Math.ceil(highestIncome / workDays);
+};
+
+module.exports = checkUser;
---
+++ script.test.js
@@ -0,0 +1,29 @@
+const checkUser = require('./script');
+
+test('should not return daily allowance, since 3G and 1.5G does not pass', () => {
+	expect(checkUser(120000, 80000, 40000)).toBe(0);
+});
+
+test('should return daily allowance, since 3G passes', () => {
+	expect(checkUser(111500, 111500, 111500)).toBe(429);
+});
+
+test('should return daily allowance, since 1.5G passes', () => {
+	expect(checkUser(170000, 105000, 40000)).toBe(654);
+});
+
+test('should return daily allowance, since both 3G and 1.5G passes, but last years income exceeds average income', () => {
+	expect(checkUser(500000, 450000, 400000)).toBe(1924);
+});
+
+test('should return daily allowance, since both 3G and 1.5G passes, but average income exceeds last years income', () => {
+	expect(checkUser(400000, 450000, 500000)).toBe(1731);
+});
+
+test('should return daily allowance, but last years income exceeds 6G', () => {
+	expect(checkUser(720000, 480000, 240000)).toBe(2573);
+});
+
+test('should return daily allowance, but average income exceeds 6G', () => {
+	expect(checkUser(700000, 700000, 700000)).toBe(2573);
+});
