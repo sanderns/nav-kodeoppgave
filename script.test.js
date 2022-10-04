616b0812d94f49ae86a513ad0b64398632e72e1f
Sander Nordhagen Svingen <sandersvingen98@gmail.com>
2022-10-04 03:22:40 +0200

    Changed order of tests

--- script.test.js
+++ script.test.js
@@ -12,12 +12,12 @@ test('should return daily allowance, since 1.5G passes', () => {
 	expect(checkUser(170000, 105000, 40000)).toBe(654);
 });
 
-test('should return daily allowance, since both 3G and 1.5G passes, but last years income exceeds average income', () => {
-	expect(checkUser(500000, 450000, 400000)).toBe(1924);
-});
-
 test('should return daily allowance, since both 3G and 1.5G passes, but average income exceeds last years income', () => {
 	expect(checkUser(400000, 450000, 500000)).toBe(1731);
+});
+
+test('should return daily allowance, since both 3G and 1.5G passes, but last years income exceeds average income', () => {
+	expect(checkUser(500000, 450000, 400000)).toBe(1924);
 });
 
 test('should return daily allowance, but last years income exceeds 6G', () => {
