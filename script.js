4e7881ae7e2db43247ce0339ad17b12ebe1b3a68
Sander Nordhagen Svingen <sandersvingen98@gmail.com>
2022-10-05 14:14:15 +0200

    Edited comments

--- script.js
+++ script.js
@@ -5,11 +5,12 @@
 //	sanderns on Github
 //
 //
-//	Jeg skriver alt i filen på Engelsk, fordi det er en vane
+//	Jeg skriver mye i filen på Engelsk, fordi det er en vane
 // 	jeg har tilegnet meg. Jeg tilegnet meg vanen her fordi jeg
 //	synes at god kode må gjøre seg selv forstått, og det er ikke
 //	nødvendigvis at mine fremtidige kolleger eller de som skal
 //	vedlikeholde koden kan Norsk like godt.
+//	For oppgaven her skriver jeg kommentarer som denne på Norsk.
 //
 //
 
@@ -17,8 +18,8 @@ const baseAmount = 111477;
 const workDays = 260;
 
 const checkEligibility = (y21, y20, y19) => {
-	// I could also take in an object or array as the argument,
-	// and then desctructure it into three different variables.
+	// Jeg kunne også ha tatt inn et objekt eller et array som
+	// argument, så desctructure det til tre forskjellige variabler.
 
 	// Variables
 	const totalIncome = y21 + y20 + y19;
